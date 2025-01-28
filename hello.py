453ce1c641a4bf357d1e316b792ae3626245d425
Nithesh Reddy Mylari Reddy <91372230+Nryreddy@users.noreply.github.com>
2025-01-28 14:21:28 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-
-st.write('Hello there, welcome')
-fav_movie = st.text_input('Which is your favourite movie?')
-
-st.write(f"Your favourite movie is {fav_movie}")
-
